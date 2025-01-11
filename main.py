799505dfb21dca4da0a24c0db2bf440da56e986d
Vinh Le <lethanhvinh95@gmail.com>
2025-01-11 20:24:12 +0200

    using model to generate YouTube search query

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from pydantic_ai import Agent
+import os
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-agent = Agent(
-  model="google-gla:gemini-2.0-flash-exp",
-  system_prompt="You are a helpful assistant that can answer questions related to Tennis",
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  system_instruction="""
+  You are a helpful assistant that can format the input into a YouTube search query.
+  The query should be used to find a video tutorial to improve the technique for the player.
+  For example: "how to improve backhand for beginner tennis player",
+  """
 )
 
-result = agent.run_sync("I want to improve my forehand. I have an Eastern grip. Give me links to 5 YouTube video to improve.")
+# Get input for YouTube search video
+keywords = [
+  "Intermediate",
+  "Forehand",
+  "Eastern grip",
+]
 
-print(result)
+response = model.generate_content(
+  f"""
+  Use following keywords to generate a YouTube search query:
+  {", ".join(keywords)}
+  """
+)
+
+print(response.text)
