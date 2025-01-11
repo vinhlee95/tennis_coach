fdbe5bd4587cb8eb550cc6e67f3808ace25b8a17
Vinh Le <lethanhvinh95@gmail.com>
2025-01-11 11:28:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from pydantic_ai import Agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(
+  model="google-gla:gemini-2.0-flash-exp",
+  system_prompt="You are a helpful assistant that can answer questions related to Tennis",
+)
+
+result = agent.run_sync("I want to improve my forehand. I have an Eastern grip. What should I do?")
+
+print(result)
